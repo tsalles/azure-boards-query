2882904b46ee27b5bccbb9a57c4609436c198b2f
Thiago Salles <thiagosalles@microsoft.com>
2025-08-19 08:22:50 -0300

    Deployment script

---
+++ deploy.ps1
@@ -0,0 +1,12 @@
+
+$resourceGroup = $args[0]
+$functionAppName = $args[1]
+
+$FolderPath = Get-Location
+$FuncIgnorePath = Join-Path $FolderPath ".funcignore"
+$Destination = Join-Path $FolderPath "app.zip"
+$Exclude = Import-Csv -Path $FuncIgnorePath -Header "Exclude"
+$Files = Get-ChildItem -Path $FolderPath -Exclude $Exclude.Exclude
+Compress-Archive -Path $Files -DestinationPath $Destination -CompressionLevel Fastest
+
+az functionapp deployment source config-zip -g $resourceGroup -n $functionAppName --src $Destination
