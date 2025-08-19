a8cef00475d04586db5eb8313c6a85222468e670
Thiago Salles <thiagosalles@microsoft.com>
2025-08-19 08:24:13 -0300

    Fix for existig archive

--- deploy.ps1
+++ deploy.ps1
@@ -2,9 +2,13 @@
 $resourceGroup = $args[0]
 $functionAppName = $args[1]
 
+
 $FolderPath = Get-Location
 $FuncIgnorePath = Join-Path $FolderPath ".funcignore"
 $Destination = Join-Path $FolderPath "app.zip"
+if (Test-Path $Destination) {
+    Remove-Item $Destination -Force
+}
 $Exclude = Import-Csv -Path $FuncIgnorePath -Header "Exclude"
 $Files = Get-ChildItem -Path $FolderPath -Exclude $Exclude.Exclude
 Compress-Archive -Path $Files -DestinationPath $Destination -CompressionLevel Fastest
